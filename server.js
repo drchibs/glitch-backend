aa01afc5e6ad953eea6a118fbb1c03dd66aec1f4
drchibs <richarddennis789@gmail.com>
2021-01-08 09:18:24 +0100

    Single server endpoint up and running

---
+++ server.js
@@ -0,0 +1,69 @@
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
+const csv = require("csvtojson");
+const cryptoRandomString = require("crypto-random-string");
+const xss = require("xss-clean");
+
+const app = express();
+
+app.use(morgan("dev"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cors());
+app.use(xss());
+
+const port = 3000;
+
+/**
+ * for the lack of a better name for the handler
+ * @returns a JSON object containing the data from the csv file in json format and a random identifier
+ */
+const doTheWork = async (req, res) => {
+	//get the 2 required params
+	const file = req.body.csv.url;
+	const fields = req.body.csv.select_fields;
+
+	try {
+		//csvtojson
+		const json = await csv().fromFile(file);
+		//unique identifier
+		const identifier = cryptoRandomString({
+			length: 32,
+			type: "alphanumeric",
+		});
+		//placeholder for new truncated json data based on selected_fields
+		const selectedData = [];
+
+		//assign a new object- target: selectedData, source: fields.map returning json[field]
+		if (fields) {
+			for (let row in json) {
+				selectedData.push(
+					Object.assign(
+						{},
+						...fields.map((key) => ({ [key]: json[row][key] }))
+					)
+				);
+			}
+		}
+
+		//send a json response in the required format
+		res.status(200).json({
+			conversion_key: identifier,
+			json: fields ? selectedData : json,
+		});
+	} catch (err) {
+		res.status(400).json({
+			status: "fail",
+			message: err.message,
+		});
+	}
+};
+
+//setup the single endpoint for access
+app.post("/", doTheWork);
+
+//start the server
+const server = app.listen(port, () => {
+	console.log(`Glitch Backend App running on port ${port}...`);
+});
